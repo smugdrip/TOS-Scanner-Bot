c9e8ba296d2c6a86dc512f9012b5ab8640ee2f06
smugdrip <jpmcb141@gmail.com>
2025-07-13 09:28:37 -0400

    adding popular page

---
+++ frontend/src/components/Popular.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+
+function Popular() {
+  return (
+    <>
+    <Navbar/>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="card p-4">
+          <h1>
+            popular page here
+          </h1>
+
+
+        </div>
+
+      </div>
+
+    </div>
+    </>
+  );
+}
+export default Popular;
